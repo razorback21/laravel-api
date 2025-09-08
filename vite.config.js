f7be0fdb7589e5d301438ba7b1a644e694cc23a9
Donz <razorback21@gmail.com>
2025-09-08 11:48:15 +0800

    added hmr

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+    server: {
+        host: true,
+        port: 5177,
+        strictPort: true,
+        origin: "http://localhost:5177",
+        cors: true,
+        hmr: {
+            port: 5177,
+            host: "localhost",
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
         tailwindcss(),
